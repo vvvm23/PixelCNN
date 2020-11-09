023143f4ee56d80b55ab5f02d9944b7ce0ec4a11
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-11-09 11:27:53 +0000

    Adds evaluation image saving

--- train.py
+++ train.py
@@ -2,13 +2,16 @@ import torch
 import torch.nn as nn
 import torchvision
 
+import copy
+import time
 from tqdm import tqdm
 
 from model import PixelCNN
 
 TRY_CUDA = True
-BATCH_SIZE = 128
+BATCH_SIZE = 256
 NB_EPOCHS = 25
+MODEL_SAVING = True
 
 def get_device():
     if TRY_CUDA == False:
@@ -32,6 +35,10 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, s
 optim = torch.optim.Adam(model.parameters())
 crit = nn.CrossEntropyLoss()
 
+save_id = int(time.time())
+best_loss = 999.
+best_model = copy.deepcopy(model.state_dict())
+
 for ei in range(NB_EPOCHS):
     print(f"\n> Epoch {ei+1}/{NB_EPOCHS}")
     train_loss = 0.0
@@ -51,7 +58,7 @@ for ei in range(NB_EPOCHS):
 
     model.eval()
     with torch.no_grad():
-        for x, h in tqdm(test_loader):
+        for i, (x, h) in enumerate(tqdm(test_loader)):
             optim.zero_grad()
             x, h = x.to(device), h.to(device)
             target = (x[:,0]*255).long()
@@ -60,5 +67,16 @@ for ei in range(NB_EPOCHS):
             loss = crit(pred.view(BATCH_SIZE, 256, -1), target.view(BATCH_SIZE, -1))
             eval_loss += loss.item()
 
+            if i == 0:
+                img = torch.cat([x, pred], dim=0)
+                torchvision.utils.save_image(img, f"samples/pixelcnn-{ei}.png")
+
     print(f"> Training Loss: {train_loss / len(train_loader)}")
     print(f"> Evaluation Loss: {eval_loss / len(test_loader)}")
+    
+    torch.save(model.state_dict(), f"models/{save_id}-{ei}-pixelcnn.pt")
+    if eval_loss / len(test_loader) < best_loss:
+        best_model = copy.deepcopy(model.state_dict())
+        best_loss = eval_loss / len(test_loader)
+
+torch.save(best_model, f"models/{save_id}-best-pixelcnn.pt")
